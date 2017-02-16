1fe48582bf6b344f23b3c07a23cc741ef9b071e2
LtDugdale <mnt_xion@hotmail.com>
2017-02-16 20:26:06 +0000

    Initial commit

---
+++ src/DictionaryModel.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+/**
+ * Created by mnt_x on 16/02/2017.
+ */
+public class DictionaryModel implements DictionaryModelInterface {
+
+    public DictionaryModel(String dictionaryFile) throws java.io.IOException{
+
+    }
+
+    public DictionaryModel() throws java.io.IOException{
+
+    }
+
+    @Override
+    public List<String> getMessage() {
+        return null;
+    }
+
+    @Override
+    public void addCharacter(char key) {
+
+    }
+
+    @Override
+    public void removeLastCharacter() {
+
+    }
+
+    @Override
+    public void nextMatch() {
+
+    }
+
+    @Override
+    public void acceptWord() {
+
+    }
+}
